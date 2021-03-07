db0b5f03fbbe2482457a20e4b85aee90d7442e0e
souleymane kone <desolo74@gmail.com>
2021-03-06 20:23:30 -0800

    updateVercel

--- src/config.js
+++ src/config.js
@@ -1,10 +1,17 @@
-
-
-const API_ENDPOINT = 'https://frozen-earth-36761.herokuapp.com'
-
-//const API_ENDPOINT = 'http://localhost:9090'
+const API_ENDPOINT = 'https://frozen-earth-36761.herokuapp.com/'
 
 export {API_ENDPOINT};
+
+
+// const API_ENDPOINT = 'https://frozen-earth-36761.herokuapp.com'
+
+
+// export {API_ENDPOINT};
+
+// export default {
+//     API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
+//     API_KEY: process.env.REACT_APP_API_KEY,
+//   }
 
 //const API_ENDPOINT = 'http://localhost:9090'
 //https://frozen-earth-36761.herokuapp.com/
